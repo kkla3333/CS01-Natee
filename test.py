cd9ca4d860dd209bc9eba63efd550a31d097e5e3
kkla3333 <136313777+kkla3333@users.noreply.github.com>
2023-07-23 23:07:37 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,27 @@
-print('natee')
+# # ข้อ1
+# number=int(input('enter'))
+# x=0
+# for num in range(number+1):
+#     x+=num
+# print(x)
+# # ข้อ2
+# time=1
+# numbers=int(input('enter'))
+# for num in range(10):
+#     print(numbers*time)
+#     time=time+1
+# # ข้อ3
+# times=0
+# numb=int(input('enter'))
+# for nums in range (numb):
+#     if times%2 !=0:
+#         print(times)
+#     times+=1
+# ข้อ4
+timess=1
+numbs=int(input('enter'))
+for numbersss in range (numbs):
+    print(timess*'*')
+    timess+=1
+
+    
