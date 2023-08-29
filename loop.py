35d17c0ff1f0298600c2d1f0fe5eab6a16df5202
kkla3333 <136313777+kkla3333@users.noreply.github.com>
2023-08-29 23:44:02 +0700

    .

---
+++ loop.py
@@ -0,0 +1,33 @@
+# while True:
+#     num=int(input('enter number'))
+#     if num==-1:
+#         print('stopped')
+#         break
+#ข้อ2
+# total=0
+# for num in range(10):
+#     number=int(input('enter number'))
+#     if number<0:
+#         continue
+#     total=total+number
+# print(total)
+# ข้อ3
+# number=int(input('enter number'))
+# number1=int(input('enter number'))
+# number2=int(input('enter number'))
+# if number>number1 and number>number2:
+#     print(number)
+# elif number1>number and number1>number2:
+#     print(number1)
+# elif number2>number1 and number2>number:
+#     print(number2)
+# else:
+#     pass
+#ข้อ4
+def addnumber (x,y):
+    print(x+y)
+addnumber(1,2)
+
+
+
+
---
+++ whileloop.py
@@ -0,0 +1,42 @@
+# 1
+# while True:
+#     num=int(input('enter integer'))
+#     print(num)
+#     if num==0:
+#         break
+#2
+# num=0
+# while True:
+#     numb=int(input('enter integer'))
+#     num+=numb
+#     print(num)
+#     if num>100:
+#         break
+#3
+# num=0
+# while True:
+#     numb=int(input('enter integer'))
+#     num+=1
+#     print(num)
+#     if numb==0:
+#         break
+#4
+# num=int(input('enter number'))
+# time=0
+# while time<num:
+#     print("*", end="")
+#     time+=1
+#5
+num=int(input('enter integer'))
+while num<1000:
+    num=num*2
+    print(num)
+    
+
+ 
+
+
+
+
+
+
